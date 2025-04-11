5cd0d6012ba6d08051f9c066b876377b38e5cd18
wenjiu <2784307979@qq.com>
2025-04-12 05:18:19 +0800

    refactor: abstract executer

--- core/src/main.rs
+++ core/src/main.rs
@@ -7,89 +7,19 @@
 use bsp::entry;
 use defmt::*;
 use defmt_rtt as _;
-use embedded_hal::pwm::SetDutyCycle;
 use panic_probe as _;
 
 // Provide an alias for our BSP so we can switch targets quickly.
 // Uncomment the BSP you included in Cargo.toml, the rest of the code does not need to change.
-use rp_pico::{
-    self as bsp,
-    hal::{self},
-};
+use rp_pico::{self as bsp};
 // use sparkfun_pro_micro_rp2040 as bsp;
-
-use bsp::hal::{
-    clocks::{Clock, init_clocks_and_plls},
-    pac,
-    sio::Sio,
-    watchdog::Watchdog,
-};
-
-/// The minimum PWM value (i.e. LED brightness) we want
-const LOW: u16 = 0;
-
-/// The maximum PWM value (i.e. LED brightness) we want
-const HIGH: u16 = 65535;
 
 #[entry]
 fn main() -> ! {
     info!("Program start");
-    let _exc = executer::Executer::new();
-    let mut pac = pac::Peripherals::take().unwrap();
-    let core = pac::CorePeripherals::take().unwrap();
-    let mut watchdog = Watchdog::new(pac.WATCHDOG);
-    let sio = Sio::new(pac.SIO);
+    let exc = executer::Executer::new();
 
-    // External high-speed crystal on the pico board is 12Mhz
-    let external_xtal_freq_hz = 12_000_000u32;
-    let clocks = init_clocks_and_plls(
-        external_xtal_freq_hz,
-        pac.XOSC,
-        pac.CLOCKS,
-        pac.PLL_SYS,
-        pac.PLL_USB,
-        &mut pac.RESETS,
-        &mut watchdog,
-    )
-    .ok()
-    .unwrap();
-
-    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
-
-    let pins = bsp::Pins::new(
-        pac.IO_BANK0,
-        pac.PADS_BANK0,
-        sio.gpio_bank0,
-        &mut pac.RESETS,
-    );
-
-    // Init PWMs
-    let mut pwm_slices = hal::pwm::Slices::new(pac.PWM, &mut pac.RESETS);
-
-    // Configure PWM4
-    let pwm = &mut pwm_slices.pwm4;
-    pwm.set_ph_correct();
-    pwm.enable();
-
-    // Output channel B on PWM4 to GPIO 25
-    let channel = &mut pwm.channel_b;
-    channel.output_to(pins.led);
-
-    loop {
-        // Ramp brightness up
-        info!("up!");
-        for i in LOW..=HIGH {
-            delay.delay_us(8);
-            let _ = channel.set_duty_cycle(i);
-        }
-
-        // Ramp brightness down
-        info!("down!");
-        for i in (LOW..=HIGH).rev() {
-            delay.delay_us(8);
-            let _ = channel.set_duty_cycle(i);
-        }
-    }
+    exc.main_loop();
 }
 
 // End of file
--- executer/src/lib.rs
+++ executer/src/lib.rs
@@ -1,14 +1,94 @@
 #![no_std]
 #![no_main]
 
-use defmt::info;
+use cortex_m::delay::Delay;
+use defmt::*;
+use defmt_rtt as _;
+use embedded_hal::pwm::SetDutyCycle;
+use panic_probe as _;
+
+use rp_pico::{
+    self as bsp,
+    hal::{self, pwm::Slices},
+};
+
+use bsp::hal::{
+    clocks::{Clock, init_clocks_and_plls},
+    pac,
+    sio::Sio,
+    watchdog::Watchdog,
+};
 
 pub struct Executer {
-    // Add fields here
+    delay: Delay,
+    pins: bsp::Pins,
+
+    pwm_slices: Slices,
 }
 
 impl Executer {
-    pub fn new() {
-        info!("Executer::new");
+    pub fn new() -> Self {
+        let mut pac = pac::Peripherals::take().unwrap();
+        let core = pac::CorePeripherals::take().unwrap();
+        let mut watchdog = Watchdog::new(pac.WATCHDOG);
+        let sio = Sio::new(pac.SIO);
+
+        let external_xtal_freq_hz = 12_000_000u32;
+        let clocks = init_clocks_and_plls(
+            external_xtal_freq_hz,
+            pac.XOSC,
+            pac.CLOCKS,
+            pac.PLL_SYS,
+            pac.PLL_USB,
+            &mut pac.RESETS,
+            &mut watchdog,
+        )
+        .ok()
+        .unwrap();
+
+        let delay = Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
+
+        let pins = bsp::Pins::new(
+            pac.IO_BANK0,
+            pac.PADS_BANK0,
+            sio.gpio_bank0,
+            &mut pac.RESETS,
+        );
+
+        let pwm_slices = hal::pwm::Slices::new(pac.PWM, &mut pac.RESETS);
+
+        info!("Executer initialized");
+
+        Self {
+            delay,
+            pins,
+            pwm_slices,
+        }
+    }
+
+    pub fn main_loop(mut self) -> ! {
+        let pwm = &mut self.pwm_slices.pwm4;
+        pwm.set_ph_correct();
+        pwm.enable();
+
+        let channel = &mut pwm.channel_b;
+        channel.output_to(self.pins.led);
+
+        const LOW: u16 = 0;
+        const HIGH: u16 = 65535;
+
+        loop {
+            info!("up!");
+            for i in LOW..=HIGH {
+                self.delay.delay_us(8);
+                let _ = channel.set_duty_cycle(i);
+            }
+
+            info!("down!");
+            for i in (LOW..=HIGH).rev() {
+                self.delay.delay_us(8);
+                let _ = channel.set_duty_cycle(i);
+            }
+        }
     }
 }
